e60eb6ba5ac8cc70984f2c903d2170d907f20c4e
Alex Aegwynn <1549777384@qq.com>
2018-06-26 09:17:29 +0800

    commit

---
+++ WebSite/Web/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.ViewModels
+{
+    public class CategoryViewModel
+    {
+        /// <summary>
+        /// 分类 ID
+        /// </summary>
+        public int CategoryID { get; set; }
+
+        /// <summary>
+        /// 分类名称
+        /// </summary>
+        public string CategoryName { get; set; }
+
+        /// <summary>
+        /// true 高亮，false 正常
+        /// </summary>
+        public bool State { get; set; }
+    }
+}
