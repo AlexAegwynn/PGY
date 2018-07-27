09d6ee6b95c81611101e7bdb6e9612b813da7aac
Alex Aegwynn <1549777384@qq.com>
2018-07-27 17:07:47 +0800

    commit

---
+++ NewsWebsite/Data/DContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Model;
+
+namespace Data
+{
+    public class DContent
+    {
+        public static List<MContent> GetContents()
+        {
+            string sql = @" SELECT * FROM wz_Content WHERE Origin = '今日头条' ";
+        }
+    }
+}
