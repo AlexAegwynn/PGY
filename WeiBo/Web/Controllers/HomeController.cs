f9f5efa8199a8f87a1413beac91ecca73cb3615f
Alex Aegwynn <1549777384@qq.com>
2018-08-02 11:01:52 +0800

    commit wb

--- WeiBo/Web/Controllers/HomeController.cs
+++ WeiBo/Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
@@ -11,10 +12,23 @@ namespace Web.Controllers
         public ActionResult Index(int catid = 0)
         {
             List<Model.MContent> list = Logic.LContent.GetContents();
+            List<VMContent> vList = new List<VMContent>();
+
+            foreach (var item in list)
+            {
+                VMContent vModel = new VMContent
+                {
+                    ArticleID = item.ArticleID,
+                    DomainID = item.DomainID,
+                    Conten = item.Conten,
+                    ImgUrl = item.ImgUrl
+                };
+                vList.Add(vModel);
+            }
 
             ViewBag.CatID = catid;
 
-            return View();
+            return View(vList);
         }
     }
 }
