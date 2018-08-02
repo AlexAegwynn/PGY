740e5b0a2cff14c5e1c0b9898e8108c7d3d1d3a5
Alex Aegwynn <1549777384@qq.com>
2018-08-02 09:41:58 +0800

    commit

---
+++ WeiBo/Data/DContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DContent
+    {
+        public long ArticleID { get; set; } = 0;
+
+        public int DomainID { get; set; } = 1;
+
+        public string Conten { get; set; } = string.Empty;
+
+        public string ImgUrl { get; set; } = string.Empty;
+    }
+}
