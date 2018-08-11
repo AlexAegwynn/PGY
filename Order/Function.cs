5fc5f2ee7114c8a0643eed80740902a36b411eeb
Alex Aegwynn <1549777384@qq.com>
2018-08-11 18:02:45 +0800

    commit

---
+++ Order/Function.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CM.Model;
+using System.Reflection;
+using System.Net;
+using System.IO;
+
+namespace CM.WebBT
+{
+	public class Function:TB.HttpAPI.TBPublic.TbBase
+	{
+		public Jayrock.Json.JsonObject GetJsonObjectBaseS(string result)
+		{
+			return GetJsonObjectBase(result);
+		}
+
+		public Jayrock.Json.JsonArray GetJsonArrayS(string getStr)
+		{
+			return GetJsonArrayBase(getStr);
+		}
+
+
+		public bool GetHtmlString_UTF8_Post_Base(string cookiesStr, IDictionary<string, string> parameters, ref MurlMsg urlMsg)
+		{
+			string htmlStr = urlMsg.HtmlStr;
+			string returnUrl = urlMsg.ReturnUrl;
+			bool isOK = GetHtmlString_UTF8_Post_Base(urlMsg.SubmitUrl, cookiesStr, parameters, ref htmlStr, ref returnUrl);
+			urlMsg.HtmlStr = htmlStr;
+			urlMsg.ReturnUrl = returnUrl;
+			return isOK;
+		}
+
+		public bool GetHtmlString_UTF8_Get_Base(string cookiesStr, ref MurlMsg urlMsg)
+		{
+			string htmlStr = urlMsg.HtmlStr;
+			string returnUrl = urlMsg.ReturnUrl;
+			bool isOK = GetHtmlString_UTF8_Get_Base(urlMsg.SubmitUrl, cookiesStr, ref htmlStr, ref returnUrl);
+			urlMsg.HtmlStr = htmlStr;
+			urlMsg.ReturnUrl = returnUrl;
+			return isOK;
+		}
+
+        
+        public bool GetHtmlString_Default_Get(string cookiesStr, ref MurlMsg urlMsg)
+        {
+            string htmlStr = urlMsg.HtmlStr;
+            string returnUrl = urlMsg.ReturnUrl;
+            bool isOK = GetHtmlString_Default_Get_Base(urlMsg.SubmitUrl, cookiesStr, ref htmlStr, ref returnUrl);
+            urlMsg.HtmlStr = htmlStr;
+            urlMsg.ReturnUrl = returnUrl;
+            return isOK;
+        }
+
+		public string GetHtmlString_UTF8_Get_New(string cookiesStr, ref MurlMsg urlMsg)
+		{
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(urlMsg.SubmitUrl);
+			if (cookiesStr != "")
+				request.Headers[HttpRequestHeader.Cookie] = cookiesStr;
+
+			request.Method = "Get";
+            //request.Host = "www.360doc.com";
+            request.Host = "www.toutiao.com";
+            //request.Referer = "http://www.360doc.com/index.html";
+            request.Referer = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36";
+
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();//执行请求，获取响应对象
+			Stream stream = response.GetResponseStream(); //获取响应流
+			StreamReader sr = new StreamReader(stream); //创建流读取对象
+			string responseHTML = sr.ReadToEnd();//读取响应流                      
+			urlMsg.HtmlStr = responseHTML;
+			response.Close();//关闭响应流
+			//int strleght = output.Length;
+			return urlMsg.HtmlStr;
+		}
+
+        #region 获取 Cookies
+        /// <summary>
+        /// 获取 Cookies
+        /// </summary>
+        public bool GetCookies(System.Uri urlObject, string url, ref string cookies)
+        {
+            if (urlObject != null)
+            {
+                url = urlObject.AbsoluteUri;
+            }
+            return GetCookiesStone(url, ref cookies);
+        }
+        #endregion
+
+        public string ListToJson<T>(IList<T> list)
+        {
+            StringBuilder Json = new StringBuilder();
+            Json.Append("[");
+            if (list.Count > 0)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    T obj = Activator.CreateInstance<T>();
+                    PropertyInfo[] pi = obj.GetType().GetProperties();
+                    Json.Append("{");
+
+                    for (int j = 0; j < pi.Length; j++)
+                    {
+                        Type type;
+                        object o = pi[j].GetValue(list[i], null);
+                        string v = string.Empty;
+                        if (o != null)
+                        {
+                            type = o.GetType();
+                            v = o.ToString();
+                        }
+                        else
+                        {
+                            type = typeof(string);
+                        }
+                        //if (pi[i].Name.ToString() != "NumIID")
+                        v = v.Replace("\r\n", "");
+                        Json.Append("\"" + pi[j].Name.ToString() + "\":" + StringFormat(v, type));
+                        if (j < pi.Length - 1)
+                        {
+                            Json.Append(",");
+                        }
+                    }
+                    Json.Append("}");
+                    if (i < list.Count - 1)
+                    {
+                        Json.Append(",");
+                    }
+                }
+            }
+            Json.Append("]");
+            return Json.ToString();
+        }
+
+        /// <summary>
+        /// 格式化字符型、日期型、布尔型
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static string StringFormat(string str, Type type)
+        {
+            if (type != typeof(string) && string.IsNullOrEmpty(str))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(string))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(DateTime))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(bool))
+            {
+                str = str.ToLower();
+            }
+            else if (type == typeof(byte[]))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(Guid))
+            {
+                str = "\"" + str + "\"";
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// 格式化字符型、日期型、布尔型
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string StringFormatS(string str, Type type)
+        {
+            if (type != typeof(string) && string.IsNullOrEmpty(str))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(string))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(DateTime))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(bool))
+            {
+                str = str.ToLower();
+            }
+            else if (type == typeof(byte[]))
+            {
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(Guid))
+            {
+                str = "\"" + str + "\"";
+            }
+            return str;
+        }
+
+		#region 【通用方法】错误写入文本
+		public void WriteTxt(string Message)
+		{
+			string pathFile = System.Windows.Forms.Application.StartupPath + @"\Error";
+			if (!Directory.Exists(pathFile))
+				Directory.CreateDirectory(pathFile);
+			string fileName = System.Windows.Forms.Application.StartupPath + @"\Error\"+DateTime.Now.ToString("yyyyMM")+".txt";
+			FileStream fs = new FileStream(fileName, FileMode.Append);
+			StreamWriter sw = new StreamWriter(fs);
+			sw.WriteLine(Message + "\r\n" + DateTime.Now);
+			sw.Flush();
+			sw.Close();
+			fs.Close();
+		}
+		#endregion
+	}
+}
---
+++ Order/MurlMsg.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CM.Model
+{
+    /// <summary>
+    /// 实体类，新版直通车网页流方式获取到的信息
+    /// </summary>
+	[Serializable]
+	public class MurlMsg
+	{
+		//{"code":"600","msg":["包含了不属于该客户的关键词Id, 请去掉。"],"result":null,"hostName":"newbpfe5.vm.kgb.cm3"} --> 批量改价时发生，即该关键词 不存在 或 已被删除
+		//{"code":"600","msg":["关键词不能为空。"],"result":null,"hostName":"newbpfe1.vm.kgb.cm3"} --> 单个改价时发生，即该关键词 不存在 或 已被删除
+		private int _Code = 200;
+		/// <summary>
+		/// 信息代码，正常 =【200】、出现验证码 =【701】
+		/// <para>【600】=【用户未登录或者登录过期】</para>
+		/// </summary>
+		public int Code { set { _Code = value; } get { return _Code; } }
+
+		private string _Msg = "";
+		/// <summary>
+		/// 提示信息，正常 = ""
+		/// </summary>
+		public string Msg { set { _Msg = value; } get { return _Msg; } }
+
+		private string _HostName = "";
+		/// <summary>
+		/// 主机名，如：newbpfe1.vm.kgb.cm6
+		/// </summary>
+		public string HostName { set { _HostName = value; } get { return _HostName; } }
+
+        private string _IP = "";
+        /// <summary>
+        /// 代理IP
+        /// </summary>
+        public string IP { set { _IP = value; } get { return _IP; } }
+
+        private string _Port = "";
+        /// <summary>
+        /// 代理端口
+        /// </summary>
+        public string Port { set { _Port = value; } get { return _Port; } }
+
+		private string _Result = "";
+		/// <summary>
+		/// 返回 Json 信息
+		/// </summary>
+		public string Result { set { _Result = value; } get { return _Result; } }
+
+		private string _SubmitUrl = "";
+		/// <summary>
+		/// 提交的网址
+		/// </summary>
+		public string SubmitUrl { set { _SubmitUrl = value; } get { return _SubmitUrl; } }
+
+		private string _HtmlStr = "";
+		/// <summary>
+		/// 网页回传内容
+		/// </summary>
+		public string HtmlStr { set { _HtmlStr = value; } get { return _HtmlStr; } }
+
+		private string _ReturnUrl = "";
+		/// <summary>
+		/// 网页 输入网址 或 回传网址
+		/// </summary>
+		public string ReturnUrl { set { _ReturnUrl = value; } get { return _ReturnUrl; } }
+
+		private int _ResultType = 0;
+		/// <summary>
+		/// 操作结果判断：未知错误=【-1】、正常=【0】、Cookies失效=【1】、出现验证码=【2】、Result字符串分析出错=【3】、错误次数太多，停止操作=【4】、重新开始操作=【5】、操作账号打开失败=【6】、页面不存在=【7】
+		/// </summary>
+		public int ResultType { set { _ResultType = value; } get { return _ResultType; } }
+
+		private bool _IsError = false;
+		/// <summary>
+		/// 操作结果是否出错：默认为否
+		/// </summary>
+		public bool IsError { set { _IsError = value; } get { return _IsError; } }
+
+		private bool _IsNormal = true;
+		/// <summary>
+		/// 操作内容是否正常：默认为是
+		/// </summary>
+		public bool IsNormal { set { _IsNormal = value; } get { return _IsNormal; } }
+
+		#region 验证码用
+
+		private string _Group = "";
+		/// <summary>
+		/// 【group】【验证码用】
+		/// </summary>
+		public string Group { set { _Group = value; } get { return _Group; } }
+
+		private string _Rid = "";
+		/// <summary>
+		/// 【rid】【验证码用】
+		/// </summary>
+		public string Rid { set { _Rid = value; } get { return _Rid; } }
+
+		private string _SessionID = "";
+		/// <summary>
+		/// 【sessionId】【验证码用】
+		/// </summary>
+		public string SessionID { set { _SessionID = value; } get { return _SessionID; } }
+
+		private string _CheckCode = "";
+		/// <summary>
+		/// 输入的验证码【验证码用】
+		/// </summary>
+		public string CheckCode { set { _CheckCode = value; } get { return _CheckCode; } }
+
+		private string _CheckCodeUrl = "";
+		/// <summary>
+		/// 验证码图片网址【验证码用】
+		/// </summary>
+		public string CheckCodeUrl { set { _CheckCodeUrl = value; } get { return _CheckCodeUrl; } }
+
+		#endregion
+	}
+}
